a2dd87aa4e8779054600fb5df6912bb4280aa4b8
daniel <simpledaniel.1818@gmail.com>
2020-03-16 22:53:04 +0300

    get started with controller and model

---
+++ models/Bootcamp.js
@@ -0,0 +1,79 @@
+const mongoose = require("mongoose");
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter Name"],
+    unique: true,
+    trime: true,
+    maxlength: [50, "Name Cannot be more then 50 charahcter"]
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, "Please Enter Description"],
+    maxlength: [500, "Description Cannot br more then 500 charahcter"]
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      "Please use a valid URL with HTTP or HTTPS"
+    ]
+  },
+  phone: {
+    type: String,
+    maxlength: [20, "Phone number can not be longer than 20 characters"]
+  },
+  email: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email"
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"]
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ["Point"]
+    },
+    coordinates: {
+      type: [Number],
+      index: "2dsphere"
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
+  careers: {
+    // Array of strings
+    type: [String],
+    required: true,
+    enum: [
+      "Web Development",
+      "Mobile Development",
+      "UI/UX",
+      "Data Science",
+      "Business",
+      "Other"
+    ]
+  },
+  averageRating: {
+    type: Number,
+    min: [1, "Rating must be at least 1"],
+    max: [10, "Rating must can not be more than 10"]
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: "no-photo.jpg"
+  }
+});
+module.exports = mongoose.model("Bootcamp", BootcampSchema);
--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 //LOAD THE ENV FILE
 dotenv.config({ path: "./config/config.env" });
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
+//dev logging midlware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+//hable the unhandled prromises
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
+});
